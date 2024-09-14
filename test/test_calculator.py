29718640283cf85692347c1dafdc66f37337476d
Wolfrum Caeiros <miguelw.caeiros@gmail.com>
2024-09-14 19:55:04 +0200

    changed library name

--- test/test_calculator.py
+++ test/test_calculator.py
@@ -2,7 +2,7 @@ import sys
 import os
 import unittest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from source import calculator
+from source import calculator_operations
 
 class Testcalculator(unittest.TestCase):
     """
@@ -13,14 +13,14 @@ class Testcalculator(unittest.TestCase):
         """
         Test that the addition of two integers returns the correct total
         """
-        result = calculator.add(1, 2)
+        result = calculator_operations.add(1, 2)
         self.assertEqual(result, 3)
 
     def test_add_floats(self):
         """
         Test that the addition of two floats returns the correct result
         """
-        result = calculator.add('10.5', 2)
+        result = calculator_operations.add('10.5', 2)
         self.assertEqual(result, 12.5)
 
     def test_add_strings(self):
@@ -28,7 +28,7 @@ class Testcalculator(unittest.TestCase):
         Test the addition of two strings returns the two strings as one
         concatenated string
         """
-        result = calculator.add('abc', 'def')
+        result = calculator_operations.add('abc', 'def')
         self.assertEqual(result, 'abcdef')
 
     def test_add_string_and_integer(self):
@@ -36,7 +36,7 @@ class Testcalculator(unittest.TestCase):
         Test the addition of a string and an integer returns them as one
         concatenated string (in which the integer is converted to a string)
         """
-        result = calculator.add('abc', 3)
+        result = calculator_operations.add('abc', 3)
         self.assertEqual(result, 'abc3')
 
     def test_add_string_and_number(self):
@@ -44,7 +44,7 @@ class Testcalculator(unittest.TestCase):
         Test the addition of a string and a float returns them as one
         concatenated string (in which the float is converted to a string)
         """
-        result = calculator.add('abc', '5.5')
+        result = calculator_operations.add('abc', '5.5')
         self.assertEqual(result, 'abc5.5')
 
 if __name__ == '__main__':
