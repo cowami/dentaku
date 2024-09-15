02f9f2f11fb8b3de5a6fb835e40307bf8de61b0e
Wolfrum Caeiros <miguelw.caeiros@gmail.com>
2024-09-15 18:20:05 +0200

    jenkins fight

--- test/test_app.py
+++ test/test_app.py
@@ -2,7 +2,7 @@ import sys
 import os
 import pytest
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from source import app
+from source.app import app
 
 @pytest.fixture
 def client():
